17e68f852dda46e62de1dafb8c7711caefe13916
samuel.ariza251@tau.usbmed.edu.co <Anemus22>
2026-05-27 14:15:27 -0500

    Fix: agregar __init__.py y ajustar rutas

--- Backend/api/app/main.py
+++ Backend/api/app/main.py
@@ -105,4 +105,5 @@ Base.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("Backend.api.app.main:app", host="127.0.0.1", port=8000, reload=True)
+
